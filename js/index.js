235f4dcc36962e0fc62ee66be2932e15668deecd
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-09-03 00:12:45 +0600

    category show in header section from api done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('index.js file connected');
+const loadCategories = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`);
+    const data = await res.json();
+    displayCategories(data.data.news_category);
+}
+const displayCategories = (categories) => {
+    categories.forEach(category => {
+        const categoryContainer = document.getElementById('category_container');
+        const catgoryName = document.createElement('li');
+        catgoryName.innerHTML = `
+        <a>${category.category_name}</a>
+        `;
+        categoryContainer.appendChild(catgoryName);
+        console.log(category);
+    });
+}
+loadCategories();
